8fd9354f50f9aed463022b3b5343f4f7a175eb72
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-09-28 14:40:28 +0200

    Refactoring

---
+++ src/ManiaxContaoJobsBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * maniax-at-work.de Contao Jobs  Bundle for Contao Open Source CMS
+ *
+ * @copyright     Copyright (c) 2022, maniax-at-work.de
+ * @author        maniax-at-work.de <https://www.maniax-at-work.de>
+ * @link          https://github.com/maniaxatwork/
+ */
+
+namespace Maniax\ContaoJobs;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ManiaxContaoJobsBundle extends Bundle
+{
+}
