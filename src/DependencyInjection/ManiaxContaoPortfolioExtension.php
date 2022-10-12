3de0a1c608a60e60a11e326bf61c7aeca79b11ff
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-10-12 16:46:08 +0200

    Initial commit

--- src/DependencyInjection/ManiaxContaoPortfolioExtension.php
+++ src/DependencyInjection/ManiaxContaoPortfolioExtension.php
@@ -17,7 +17,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ManiaxContaoPortolioExtension extends Extension
+class ManiaxContaoPortfolioExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
