ee2f610b62f8af0e27ba54f686c9409384a72f4f
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-10-21 15:50:56 +0200

    Initial commit

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -10,16 +10,56 @@ declare(strict_types=1);
  * @link          https://github.com/maniaxatwork/
  */
 
-use Maniax\ContaoPortfolio\EventListener\Contao\DCA\PortfolioItemFields;
+use Maniax\ContaoPortfolio\EventListener\Contao\DCA\PortfolioItemFiles;
 use Maniax\ContaoPortfolio\EventListener\Contao\DCA\TlModule;
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['maniax_contao_portfolio_item_list'] =
-    '{title_legend},name,category;{expert_legend:hide},cssID';
+    '{title_legend},name,type,portfolioCategory;
+    {config_legend},portfolioHeadlineTag,portfolioHeadline,portfolioSortingDefaultField,portfolioSortingDefaultDirection;
+    {expert_legend:hide},cssID'
+;
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['category'] = [
+$GLOBALS['TL_DCA']['tl_module']['fields']['portfolioCategory'] = [
     'exclude' => true,
-    'inputType' => 'checkboxWizard',
+    'inputType' => 'select',
     'options_callback' => [TlModule::class, 'onCategoryOptionsCallback'],
-    'eval' => ['multiple' => true, 'tl_class' => 'clr'],
+    'eval' => ['tl_class' => 'clr w50'],
     'sql' => 'mediumtext null',
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['portfolioHeadlineTag'] = [
+    'exclude' => true,
+    'search' => true,
+    'inputType' => 'select',
+    'options' => ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div'],
+    'eval' => ['maxlength' => 8, 'tl_class' => 'w50 clr'],
+    'sql' => "varchar(8) NOT NULL default 'h2'",
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['portfolioHeadline'] = [
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => ['allowHtml' => true, 'tl_class' => 'w50'],
+    'sql' => "varchar(255) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['portfolioSortingDefaultField'] = [
+    'exclude' => true,
+    'inputType' => 'select',
+    'options' => PortfolioItemFiles::getFields(),
+    'eval' => [
+        'tl_class' => 'w50 clr',
+    ],
+    'reference' => &$GLOBALS['TL_LANG']['tl_module']['portfolioSortingDefaultField']['fields'],
+    'sql' => "varchar(255) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['portfolioSortingDefaultDirection'] = [
+    'exclude' => true,
+    'inputType' => 'select',
+    'options' => ['ASC', 'DESC'],
+    'eval' => [
+        'tl_class' => 'w50',
+    ],
+    'sql' => "varchar(4) NOT NULL default ''",
 ];
