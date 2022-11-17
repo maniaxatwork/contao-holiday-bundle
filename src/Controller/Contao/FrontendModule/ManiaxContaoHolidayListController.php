af826e04b8324529fd020c8887dddb37186d470b
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-11-17 16:03:48 +0100

    Initial commit

--- src/Controller/Contao/FrontendModule/ManiaxContaoHolidayListController.php
+++ src/Controller/Contao/FrontendModule/ManiaxContaoHolidayListController.php
@@ -88,6 +88,8 @@ class ManiaxContaoHolidayListController extends AbstractFrontendModuleController
             $itemTemplate->doc2 = $doc2;
             $itemTemplate->doc3 = $doc3;
             $itemTemplate->doc4 = $doc4;
+            $itemTemplate->holidayStart = $holidayItem->getHolidayStart();
+            $itemTemplate->holidayStop = $holidayItem->getHolidayStop();
             $items[] = $itemTemplate->parse();
         }
 
