68a609ffc396ba941d2dfa4e3a20b218c735d1c6
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-11-18 13:08:32 +0100

    Initial commit

--- src/Resources/public/js/script.js
+++ src/Resources/public/js/script.js
@@ -5,4 +5,9 @@ $(document).ready(function() {
         $('.mod_maniax_contao_holiday_list').addClass('show');
         localStorage.setItem('holidayShown', 1);
     }
+
+    //test
+
+    $('.mod_maniax_contao_holiday_list').addClass('show');
+    // end test
 });
