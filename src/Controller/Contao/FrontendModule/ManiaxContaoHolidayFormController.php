cf5dc8dfb13bd32a678183122e1d252a67d2f1b1
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-11-22 16:04:09 +0100

    Initial commit

--- src/Controller/Contao/FrontendModule/ManiaxContaoHolidayFormController.php
+++ src/Controller/Contao/FrontendModule/ManiaxContaoHolidayFormController.php
@@ -231,7 +231,7 @@ class ManiaxContaoHolidayFormController extends AbstractFrontendModuleController
                     $tmstp1 = strtotime($date1[2]."-".$date1[1]."-".$date1[0]);
                     $date2 = explode(".",$duplicateFieldsData['vertretungDoc1VertretungStop_duplicate_'.$i]);
                     $tmstp2 = strtotime($date2[2]."-".$date2[1]."-".$date2[0]);
-                    $doc[] = [
+                    $doc = [
                         "doc" => $duplicateFieldsData['vertretungDoc1_duplicate_'.$i],
                         "vertretungStart" => $tmstp1,
                         "vertretungStop" => $tmstp2,
