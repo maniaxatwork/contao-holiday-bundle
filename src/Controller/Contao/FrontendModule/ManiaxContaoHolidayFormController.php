2ebe44dfd6fd8701a6075404cc2b42521fb9fb58
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-11-22 16:29:28 +0100

    Initial commit

--- src/Controller/Contao/FrontendModule/ManiaxContaoHolidayFormController.php
+++ src/Controller/Contao/FrontendModule/ManiaxContaoHolidayFormController.php
@@ -69,8 +69,14 @@ class ManiaxContaoHolidayFormController extends AbstractFrontendModuleController
         });
 
         // Vertretung 1
-        $objForm->addFormField('vertretung1Start', array(
+        $objForm->addFormField('vertretung1ContStart', array(
             'label' => &$GLOBALS['TL_LANG']['tl_maniax_contao_holiday_item']['vertretungDoc1'],
+            'inputType' => 'fieldsetStart',
+            'eval' => array('class' => 'fieldset_outer')
+        ), new \Haste\Util\ArrayPosition(\Haste\Util\ArrayPosition::BEFORE, 'vertretungDoc1'));
+
+        $objForm->addFormField('vertretung1Start', array(
+            'label' => 'Vertretungsarzt',
             'inputType' => 'fieldsetStart',
             'eval' => array('class' => 'allow-duplication duplicate-fieldset-1 duplicate-fieldset-maxRows-3')
         ), new \Haste\Util\ArrayPosition(\Haste\Util\ArrayPosition::BEFORE, 'vertretungDoc1'));
@@ -90,6 +96,10 @@ class ManiaxContaoHolidayFormController extends AbstractFrontendModuleController
             'inputType' => 'text',
             'eval' => array('rgxp' => 'date','datepicker' => true,'mandatory' => true),
         ), new \Haste\Util\ArrayPosition(\Haste\Util\ArrayPosition::BEFORE, 'vertretung1Stop'));
+
+        $objForm->addFormField('vertretung1ContStop', array(
+            'inputType' => 'fieldsetStop',
+        ), new \Haste\Util\ArrayPosition(\Haste\Util\ArrayPosition::AFTER, 'vertretung1Stop'));
 
         // Vertretung 2
         $objForm->addFormField('vertretung2Start', array(
