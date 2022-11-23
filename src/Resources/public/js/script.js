033dfcbcc53792b75a41a1283d8920955069939e
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2022-11-23 16:40:08 +0100

    Initial commit

--- src/Resources/public/js/script.js
+++ src/Resources/public/js/script.js
@@ -11,11 +11,11 @@ $(document).ready(function() {
         $('.mod_maniax_contao_holiday_list').removeClass('show');
     })
 
-    $('.holidayInfoIcon').on('click', function(e){
-        $('.mod_maniax_contao_holiday_list').removeClass('show');
-    })
-
     if( ($('.holiday_list').length) && ($('body').hasClass('start')) ){
         $('#headerimage .inside').append('<div id="holidayInfoIcon" class="holidayInfoIcon"><i class="fas fa-circle-info" aria-hidden="true"></i> Urlaub</div>');
+
+        $('.holidayInfoIcon').on('click', function(e){
+            $('.mod_maniax_contao_holiday_list').removeClass('show');
+        })
     }
 });
